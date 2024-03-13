1df0ec2b10d8dd62de1cbbcdc89e36242c1a2400
cojak <cojak@email.local>
2024-03-12 22:20:06 -0700

    solympic games leaderboard proof of concept v3.7

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const sponsorImages = {
   goblins: 'https://memedepot.com/cdn-cgi/imagedelivery/naCPMwxXX46-hrE49eZovw/4b0c2d77-33cb-450c-d087-dab3cfd22800/public',
   surfers: 'https://memedepot.com/cdn-cgi/imagedelivery/naCPMwxXX46-hrE49eZovw/cceda807-5155-4afe-337d-992ac314c200/public',
   cigs: 'https://memedepot.com/cdn-cgi/imagedelivery/naCPMwxXX46-hrE49eZovw/2d1fe668-445d-43fa-17fe-37e57c9c2800/public',
-  unsponsored: 'https://memedepot.com/cdn-cgi/imagedelivery/naCPMwxXX46-hrE49eZovw/e65eda39-0037-46e6-dc03-e28664675700/public'
+  unsponsored: 'https://memedepot.com/cdn-cgi/imagedelivery/naCPMwxXX46-hrE49eZovw/a03abb01-1256-433c-3617-89f4ae02e000/public'
   // Add more mappings as necessary
 };
 
